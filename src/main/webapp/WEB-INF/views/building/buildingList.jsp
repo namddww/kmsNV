1db4896eaf36ba537e9bd8667409e8a1c5bd84c3
sseungsoo <jangbilss@hubilon.com>
2022-02-09 18:03:13 +0900

    건물 조회

--- src/main/webapp/WEB-INF/views/building/buildingList.jsp
+++ src/main/webapp/WEB-INF/views/building/buildingList.jsp
@@ -1,1 +1,21 @@
-<div>건물조회aaaa</div>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<h1>건물 조회</h1>
+<div id="buildingList">
+    <table id="buildingListTable" border="1">
+        <thead>
+        <tr>
+            <th>건물명</th>
+            <th>지역</th>
+            <th>상세주소1</th>
+            <th>상세주소2</th>
+            <th>층수</th>
+            <th>좌표</th>
+            <th>등록일</th>
+        </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="/assets/js/building/buildingList.js"></script>
---
+++ src/main/webapp/assets/js/building/buildingList.js
@@ -0,0 +1,71 @@
+var _buildingList = {
+    $scope      : null, // 영역
+    $tableList  : null, // content form
+
+    init : function () {
+
+        this.$scope      = $("#buildingList");
+        this.$tableList  = $('#buildingListTable', this.$scope);
+
+        this.events();
+    },
+
+    events : function () {
+        const _this = this;
+        this.search(1);
+    },
+
+    search : function(page) {
+        const _this = this;
+        var param = {
+
+        };
+        $.ajax({
+            type : "GET",
+            url : "/building/search",
+            data : param,
+            success : function(res){
+                _this.dataBind(res.result);
+            },
+            error : function(XMLHttpRequest, textStatus, errorThrown){
+
+            }
+        });
+
+    },
+
+    dataBind : function(result) {
+        const _this = this;
+        _this.$tableList.find('tbody').empty();
+        if (result.list.length > 0) {
+            $.each(result.list, function (i, val) {
+                _this.$tableList.append(
+                    $('<tr/>').append(
+                        $('<td/>').text(val.buildName)
+                    ).append(
+                        $('<td/>').text(" ")
+                    ).append(
+                        $('<td/>').text(val.addr1)
+                    ).append(
+                        $('<td/>').text(val.addr2)
+                    ).append(
+                        $('<td/>').text('지상'+val.groundFloor+'/ 지하'+val.baseFloor)
+                    ).append(
+                        $('<td/>').text(" ")
+                    ).append(
+                        $('<td/>').text(val.regDate)
+                    )
+                )
+            });
+        } else {
+            _this.$tableList.append(
+                $('<tr/>').append($('<td/>').attr({colspan : 6}).text('검색결과가 없습니다.'))
+            );
+        }
+    }
+};
+
+// onload
+$(document).ready(function() {
+    _buildingList.init();
+});
